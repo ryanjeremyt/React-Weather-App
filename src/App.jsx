8a039d65f5c615f2bde6da336e4c66a796d0080f
ryanjeremyt <ryanjeremyt@gmail.com>
2025-05-18 16:16:09 -0700

    Initial commit for Vite  React App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div classNamme='app'>
+      <Weather></Weather>
+    </div>
+  )
+}
+
+export default App
